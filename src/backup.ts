6f4716e204909aa4b4271cccb4df8085536727c9
Jose <35411802+josegiufrida@users.noreply.github.com>
2023-12-08 22:02:18 -0300

    Fix Compatibility with Postgres 16

--- src/backup.ts
+++ src/backup.ts
@@ -2,6 +2,9 @@ import { exec } from "child_process";
 import { Storage, UploadOptions } from "@google-cloud/storage";
 import { unlink } from "fs";
 
+import path from "path";
+import os from "os";
+
 import { env } from "./env";
 
 const uploadToGCS = async ({ name, path }: { name: string; path: string }) => {
@@ -21,24 +24,26 @@ const uploadToGCS = async ({ name, path }: { name: string; path: string }) => {
 
 const dumpToFile = async (path: string) => {
   await new Promise((resolve, reject) => {
-    exec(
-      `pg_dump ${env.BACKUP_DATABASE_URL} -F t | gzip > ${path}`,
-      (error, _, stderr) => {
-        if (error) {
-          reject({ error: JSON.stringify(error), stderr });
-          return;
-        }
-
-        resolve(undefined);
+    exec(`pg_dump --dbname=${env.BACKUP_DATABASE_URL} --format=tar | gzip > ${path}`, (error, stdout, stderr) => {
+      if (error) {
+        reject({ error: error, stderr: stderr.trimEnd() });
+        return;
       }
-    );
+
+      if (stderr != "") {
+        reject({ stderr: stderr.trimEnd() });
+        return;
+      }
+
+      resolve(undefined);
+    });
   });
 };
 
 const deleteFile = async (path: string) => {
   await new Promise((resolve, reject) => {
     unlink(path, (err) => {
-      reject({ error: JSON.stringify(err) });
+      reject({ error: err });
       return;
     });
     resolve(undefined);
@@ -49,7 +54,7 @@ export const backup = async () => {
   let date = new Date().toISOString();
   const timestamp = date.replace(/[:.]+/g, "-");
   const filename = `${env.BACKUP_PREFIX}backup-${timestamp}.tar.gz`;
-  const filepath = `/tmp/${filename}`;
+  const filepath = path.join(os.tmpdir(), filename);
 
   await dumpToFile(filepath);
   await uploadToGCS({ name: filename, path: filepath });
